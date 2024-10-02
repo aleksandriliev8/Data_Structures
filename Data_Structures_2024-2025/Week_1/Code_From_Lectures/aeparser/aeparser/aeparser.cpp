860475598be18eb187a51128e62fed537a2b908a
Aleksandar Iliev <aleksandriliev8@gmail.com>
2024-10-03 02:42:56 +0300

    Extending the program to handle double numbers

--- Data_Structures_2024-2025/Week_1/Code_From_Lectures/aeparser/aeparser/aeparser.cpp
+++ Data_Structures_2024-2025/Week_1/Code_From_Lectures/aeparser/aeparser/aeparser.cpp
@@ -6,7 +6,8 @@
 
 struct Token
 {
-    enum TokenType { open_par, number, oper, close_par };
+    //added doubleNum as new type with index 2
+    enum TokenType { open_par, number, doubleNum, oper, close_par };
     TokenType type;
     std::string val;
 };
@@ -31,13 +32,44 @@ Token nextToken(std::istream& in)
     if (isdigit(c))
     {
         std::string num;
-        while (isdigit(c))
+        //creating boolean to track if the number is double
+        bool isDouble = false;
+        char lastSymbol = c;
+
+        //adding condition for accepting and record the '.'
+        while (isdigit(c) || c == '.')
         {
+            //adding a "lastSymbol" to track the last character in "num"
+            lastSymbol = c;
+
+            //if '.' is found we change the boolean expression to true and add the symbol to the result
+            if (c == '.') 
+            { 
+                isDouble = true;
+            }
+
             num += c;
             in.get();
             c = in.peek();
         }
-        return Token{ Token::number, num };
+
+        //depending of the customer preferences, if the number ends with '.' :
+
+        if (lastSymbol == '.') 
+        {
+            //1)it can be considered as invalid character
+            //std::cerr << "char=" << c << "\n" << std::endl;
+            //throw std::runtime_error("Invalid character");
+
+            //2)or it can be verified as an integer
+
+            //removing '.'
+            num.pop_back();
+            return Token{ Token::number, num };
+        }
+
+        //in this case the returning value depends on the value of the boolean "isDouble"
+        return isDouble ? Token{ Token::doubleNum, num } : Token{ Token::number, num };
     }
     else if (c == '(')
     {
@@ -90,7 +122,7 @@ std::vector<Token> tokenize(std::istream& in)
 
 int main()
 {
-    std::ifstream in("expression.exis");
+    std::ifstream in("expression.txt");
 
     std::vector<Token> tokens = tokenize(in);
     // do something with parsed
