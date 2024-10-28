eefd7d887affdb72949d62e42847d1f46f57c660
Aleksandar Iliev <aleksandriliev8@gmail.com>
2024-10-28 11:07:15 +0200

    Added recursive solution for binary search algorithm

---
+++ Algorithms/searching_algorithms/binary_search_rec/binary_search_rec.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+using namespace std;
+
+template <class T>
+int binarySearch(const int* arr, int len, const T& searched)
+{
+	if (len == 0)
+		return -1;
+
+	int midIndex = len / 2;
+
+	if (arr[midIndex] == searched)
+		return midIndex;
+	
+    if (arr[midIndex] < searched) 
+    {
+        int result = binarySearch(arr + midIndex + 1, len - midIndex - 1, searched);
+        return (result == -1) ? -1 : result + midIndex + 1;
+    }
+	else
+		return binarySearch(arr, midIndex, searched);
+}
+
+template <typename Iterator, typename T>
+Iterator binarySearch(Iterator left, Iterator right, const T& value)
+{
+    if (left >= right)
+        return right;
+    
+    Iterator middle = left + std::distance(left, right) / 2;
+
+    if (*middle == value)
+        return middle;
+    else if (*middle < value)
+        return binarySearch(middle + 1, right, value);
+    else
+        return binarySearch(left, middle, value);
+
+}
+
+int main()
+{
+    int arr[] = { 1, 2, 2, 6, 19, 55 };
+    cout << binarySearch(arr, 6, 19);
+}
