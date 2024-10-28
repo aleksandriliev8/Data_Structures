74c51adfb9708c1598d4ee772c3210bf8f9fa1f2
Aleksandar Iliev <aleksandriliev8@gmail.com>
2024-10-28 09:47:15 +0200

    Added iterative solution for linear search

---
+++ Algorithms/searching_algorithms/linear_search_iter.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+template <class T>
+int linearSearch(const T* arr, unsigned len, const T& searched)
+{
+    for (int i = 0; i < len; i++)
+    {
+        if (arr[i] == searched)
+            return i;
+    }
+    return -1;
+}
+
+//with iterators
+template <typename Iterator, typename T>
+Iterator linearSeearch(Iterator begin, Iterator end, const T& value)
+{
+    for (Iterator it = begin; it != end; ++it)
+    {
+        if (*it == value)
+            return it;
+    }
+    return end;
+}
+
+int main()
+{
+    int arr[] = { 1, 2, 3, 4, 5, 6, 7 };
+
+    std::cout << linearSearch<int>(arr, 7, 10);
+}
